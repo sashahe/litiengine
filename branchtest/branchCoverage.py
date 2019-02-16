880256887435c6b671814250708ede35c05ca10b
Gustaf Gunér <gustaf@guner.se>
2019-02-16 21:11:45 +0100

    Fix #36 Refactored our python script

--- branchtest/branchCoverage.py
+++ branchtest/branchCoverage.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
     outfile_name = 'result.txt'
     filenames = ['test_01.csv', 'test_02.csv', 'test_03.csv', 'test_04.csv', 'test_05.csv',
                  'test_06.csv', 'test_07.csv', 'test_08.csv', 'test_09.csv', 'test_10.csv']
-    #filenames = ['test_02.csv']
 
     for i in range(len(filenames)):
         # Load all data from a specific funciton, each column represents a branch path
@@ -21,7 +20,7 @@ if __name__ == '__main__':
 
         # Sum over columns to see what branches are untested
         data = np.sum(data, axis=0)
-        data[data > 0] = 1
+        data[np.where(data > 0)] = 1
 
         # If a branch has a sum > 0 it means it's tested somewhere
         branches = len(data)
@@ -34,7 +33,7 @@ if __name__ == '__main__':
             outfile.write("–––––––––––––––––––––––––\n")
             outfile.write("Function ID: #" + str(i+1) + "\nBranches: " + str(branches) +
                           "\nBranches covered: " + str(branchesCovered) + "\nBranch coverage: " + str(coverage) + "%\n\n")
-            # outfile.write("BranchID  -  Covered\n")
+
             for j in range(branches):
                 outfile.write("Branch " + str(j+1) +
                               ": " + str('✅' if data[j] == 1 else '❌') + '\n')
