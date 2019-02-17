efc46871ce08d3177dc199ae1d16f47fdb4bc9b7
Sasha <sasha.hellstenius@gmail.com>
2019-02-17 22:28:32 +0100

    Fix #30 Cleanup

--- tests/de/gurkenlabs/litiengine/environment/TileTest.java
+++ tests/de/gurkenlabs/litiengine/environment/TileTest.java
@@ -1,30 +1,20 @@
 package de.gurkenlabs.litiengine.environment;
 
 import static org.mockito.Mockito.mock;
-// import static org.mockito.Mockito.times;
-// import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.awt.Dimension;
 import java.awt.Point;
-// import java.awt.Graphics2D;
 import java.util.ArrayList;
 
-// import org.junit.jupiter.api.AfterAll;
-// import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import org.junit.jupiter.params.ParameterizedTest;
-// import org.junit.jupiter.params.provider.EnumSource;
-// import org.junit.jupiter.params.provider.EnumSource.Mode;
-
-// import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.environment.tilemap.MapUtilities;
 import de.gurkenlabs.litiengine.environment.tilemap.IMap;
 import de.gurkenlabs.litiengine.environment.tilemap.MapOrientation;
-// import de.gurkenlabs.litiengine.graphics.RenderType;
+
 
  public class TileTest {
   private Environment testEnvironment;
@@ -51,7 +41,6 @@ import de.gurkenlabs.litiengine.environment.tilemap.MapOrientation;
 
   @Test
   public void testGetTileOrthogonalMapInvalid() {
-    when(map.getOrientation()).thenReturn(MapOrientation.ORTHOGONAL);
     when(map.getTileWidth()).thenReturn(2);
     when(map.getTileHeight()).thenReturn(2);
     Point p = MapUtilities.getTile(this.map, -1, -1);
