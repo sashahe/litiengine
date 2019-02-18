e5f3a793c306f9d78a89ecc4b972291086c49b02
Sasha <sasha.hellstenius@gmail.com>
2019-02-19 00:55:47 +0100

    Fix #57 Add missing curly bracket to fix build failure

--- tests/de/gurkenlabs/litiengine/environment/EnvironmentTests.java
+++ tests/de/gurkenlabs/litiengine/environment/EnvironmentTests.java
@@ -150,7 +150,7 @@ public class EnvironmentTests {
     this.testEnvironment.add(entityWithEmptyTag);
 
     assertEquals(0, this.testEnvironment.getEntitiesByTag().size());
-
+  }
   /*
    * DD2480: checks if function 3's branch #4 is reached.
    * Test RenderType is null -> getEntities should not have anything removed.
