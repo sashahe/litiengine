d86a6c0cf51a9279cc69072c38e5fff5cad466f3
Sasha <sasha.hellstenius@gmail.com>
2019-02-21 17:39:07 +0100

    Fix #96 Add unit tests: canCast()

--- tests/de/gurkenlabs/litiengine/abilities/AbilityTests.java
+++ tests/de/gurkenlabs/litiengine/abilities/AbilityTests.java
@@ -3,6 +3,10 @@ package de.gurkenlabs.litiengine.abilities;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +14,10 @@ import de.gurkenlabs.litiengine.abilities.effects.Effect;
 import de.gurkenlabs.litiengine.abilities.effects.EffectTarget;
 import de.gurkenlabs.litiengine.annotation.AbilityInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
+import de.gurkenlabs.litiengine.IGameLoop;
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.attributes.Attribute;
+
 
 public class AbilityTests {
 
@@ -56,4 +64,59 @@ public class AbilityTests {
       super(ability, targets);
     }
   }
+
+  /**
+  * If the executor is dead it is not possible to cast
+  */
+  @Test
+  public void testCanCastWhenDead() {
+    Creature entity = mock(Creature.class);
+    when(entity.isDead()).thenReturn(true);
+
+    TestAbility a = new TestAbility(entity);
+    assertEquals(a.getExecutor(), entity);
+    assertFalse(!a.getExecutor().isDead());
+    assertFalse(a.canCast());
+  }
+
+  /**
+  * If the executor is alive, and
+  * If the ability has no current execution it is possible to cast
+  */
+  @Test
+  public void testCanCastWhenNoExecution() {
+    Creature entity = mock(Creature.class);
+    when(entity.isDead()).thenReturn(false);
+
+    TestAbility a = new TestAbility(entity);
+    a.setCurrentExecution(null);
+
+    assertEquals(a.getExecutor(), entity);
+    assertTrue(!a.getExecutor().isDead());
+    assertTrue(a.getCurrentExecution() == null);
+    assertTrue(a.canCast());
+  }
+
+  /**
+  * If the executor is alive, and
+  * If the execution has no execution ticks left it is possible to cast
+  */
+  @Test
+  public void testCanCastWhenNoExecutionticks() {
+    Creature entity = mock(Creature.class);
+    when(entity.isDead()).thenReturn(false);
+
+    AbilityExecution ae = mock(AbilityExecution.class);
+    when(ae.getExecutionTicks()).thenReturn(0l);
+
+    TestAbility a = new TestAbility(entity);
+    a.setCurrentExecution(ae);
+
+    assertEquals(a.getExecutor(), entity);
+    assertTrue(!a.getExecutor().isDead());
+    assertFalse(a.getCurrentExecution() == null);
+    assertTrue(a.getCurrentExecution().getExecutionTicks() == 0);
+    assertTrue(a.canCast());
+  }
 }
+;
